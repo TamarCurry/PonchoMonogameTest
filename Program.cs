f162cc63edfc4a24e7bdf20926a48d2805663ecb
streiffus <tamar.curry@gmail.com>
2016-06-17 02:04:56 -0700

    Initial commit
    Tests for running Poncho using Monogame.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Poncho;
+using PonchoMonogame;
+
+namespace PonchoMonogameTest
+{
+	/// <summary>
+	/// The main class.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			App.Init(new MonogameApp(null));
+		}
+	}
+}
